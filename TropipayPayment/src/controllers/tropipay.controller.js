f0918662e65ada1f9b812302e3b3fabfb95e2bce
fernargdev <115408504+fernargdev@users.noreply.github.com>
2024-05-10 11:29:53 -0400

    usando dominio de test

--- TropipayPayment/src/controllers/tropipay.controller.js
+++ TropipayPayment/src/controllers/tropipay.controller.js
@@ -3,62 +3,17 @@ const config = require('../utils/config')
 
 const helper = async (req, res) => {
   res.json({
-    message: 'Tropipay API',
+    message: 'Hello World!',
   })
 }
-
-// const tropipayLogin = async (req, res) => {
-//   try {
-//     const response = await axios.post(
-//       'https://www.tropipay.com/api/access/login',
-//       {
-//         email: config.TROPIPAY_EMAIL,
-//         password: config.TROPIPAY_PASSWORD,
-//       }
-//     )
-
-//     const token = response.data.token
-//     res.json({ token: token })
-//   } catch (error) {
-//     res.status(500).json({ message: error.message, error: error })
-//   }
-// }
-
-// const tropipayPay = async (req, res) => {
-//   try {
-//     const token = req.body.token
-//     const response = await axios.post(
-//       'https://www.tropipay.com/api/v2/movements/in/with_tpp_url',
-//       {
-//         reference: req.body.reference,
-//         concept: req.body.concept,
-//         description: req.body.description,
-//         amount: Math.round(req.body.amount * 100),
-//         currency: req.body.currency,
-//         lang: req.body.lang,
-//         urlSuccess: req.body.urlSuccess,
-//         urlFailed: req.body.urlFailed,
-//         urlNotification: req.body.urlNotification,
-//       },
-//       {
-//         headers: { Authorization: `Bearer ${token}` },
-//       }
-//     )
-
-//     const url = response.data.url
-//     res.json({ url: url })
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error al realizar el pago en Tropipay' })
-//   }
-// }
 
 const tropipayLogin = async () => {
   try {
     const response = await axios.post(
-      'https://www.tropipay.com/api/access/login',
+      'https://tropipay-dev.herokuapp.com/api/access/login',
       {
-        email: config.TROPIPAY_EMAIL,
-        password: config.TROPIPAY_PASSWORD,
+        email: config.TROPIPAY_TESTING_EMAIL,
+        password: config.TROPIPAY_TESTING_PASSWORD,
       }
     )
 
@@ -79,7 +34,7 @@ const tropipayPay = async (req, res) => {
     }
 
     const response = await axios.post(
-      'https://www.tropipay.com/api/v2/movements/in/with_tpp_url',
+      'https://tropipay-dev.herokuapp.com/api/v2/movements/in/with_tpp_url',
       {
         reference: req.body.reference,
         concept: req.body.concept,
